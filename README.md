63789a349b732ea8af903064421a4ae144b5dda9
jessemonroy650 <jessemonroy650@yahoo.com>
2018-11-10 20:08:45 -0700

    initial entry

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# hack-use-subdirectories
-Test of using subdirectories with Cordova/Phonegap - using `Phonegap Build`
+# Hack Display Date Time
+
+A *Phonegap Build* simple example to display the date and time using `cli-6.0.0`, and minimal CSS & Javascript. It may use the Gooogle *Text to Speech* cloud service.
+
+In addition, this example app uses `cordova-plugin-device`.
+
+* https://www.npmjs.com/package/cordova-plugin-device
+* https://www.npmjs.com/package/cordova-plugin-screen-orientation
+* https://www.npmjs.com/package/cordova-plugin-texttospeech
+
+## Reference for Date object
+
+* https://www.w3schools.com/jsref/jsref_obj_date.asp
+
+
+## Supported Voices
+
+To use the other voices the plugin will need to be updated.
+
+```
+Language 	Voice type 	Language code 	Voice name 		SSML Gender
+English (US) 	Standard 	en-US 		en-US-Standard-B 	MALE
+English (US) 	Standard 	en-US 		en-US-Standard-C 	FEMALE
+English (US) 	Standard 	en-US 		en-US-Standard-D 	MALE
+English (US) 	Standard 	en-US 		en-US-Standard-E 	FEMALE
+```
+
+A complete list of voices is available here:
+
+https://cloud.google.com/text-to-speech/docs/voices
+
+The other voices include: Dutch, English (Australia, US, UK), French, German, Italian, Japanese, Korean, Portugese, Spanish, Swedish, & Turkish. 
+
+### SSML End Points
+
+https://cloud.google.com/text-to-speech/docs/reference/rest/v1/voices/list
---
+++ js/app.js
@@ -0,0 +1,78 @@
+var app = {
+    version : '1.0.0',
+    targetEvent : 'click',
+    isCordova   : false,
+    stop        : false,
+
+    // start the "ticker", if (not app.stop)
+    kickTicker : function () {
+        if (! app.stop) {
+            setTimeout(app.updateDateTimeDisplay, 1000);
+        }
+    },
+    //
+    sayDatetime : function () {
+        var t2sDatetime = '';
+        var hourStr     = (datetime.hourStr > 12) ? (datetime.hourStr - 12) : datetime.hourStr;
+        t2sDatetime = "Today is " + datetime.dowString[datetime.dowStr] + ' ' +
+                      datetime.monthString[datetime.monthStr]  + ' ' + 
+                      datetime.dateStr + ", " + datetime.yearStr + ". The time is " +
+                      hourStr + ":" + datetime.minuteStr + ".";
+        document.getElementById('status').innerHTML = t2sDatetime;
+        //
+        if (app.isCordova) {
+            txt2SpeechPlugin.talk(t2sDatetime);
+        } else {
+            alert("not working at this time.");
+        }
+    },
+    //
+    updateDateTimeDisplay : function () {
+        datetime.get();
+        //document.getElementById('epochTime').innerHTML = datetime.epoch;
+        //  Date
+        document.getElementById('month').innerHTML     = datetime.monthString[datetime.monthStr];
+        document.getElementById('date').innerHTML      = datetime.dateStr;
+        document.getElementById('year').innerHTML      = datetime.yearStr;
+        //  Day Of the Week
+        document.getElementById('dow').innerHTML       = datetime.dowString[datetime.dowStr];
+        //  Time
+        //  https://stackoverflow.com/questions/3605214/javascript-add-leading-zeroes-to-date
+        document.getElementById('hour').innerHTML      = ('0' + datetime.hourStr).slice(-2);
+        document.getElementById('minute').innerHTML    = ('0' + datetime.minuteStr).slice(-2);
+        document.getElementById('second').innerHTML    = ('0' + datetime.secondStr).slice(-2);
+        app.kickTicker();
+    },
+    //
+    hook : function () {
+        // SHOULD HIDE THIS BUTTON IN THE BROWSER ONLY
+        document.getElementById('exitApp').addEventListener(app.targetEvent,
+            function () { 
+                if (app.isCordova) {
+                    navigator.app.exitApp();  
+                } else {
+                    app.stop = ! app.stop;
+                    app.kickTicker();
+                }
+            },
+            false);
+    },
+    //
+    onDOMContentLoaded : function () {
+        app.hook();
+        //
+        document.getElementById('appVersion').innerHTML  = app.version;
+        //
+        app.isCordova                               = (typeof window.cordova !== "undefined");
+        document.getElementById('status').innerHTML = 'is webbrowser';
+        app.updateDateTimeDisplay();
+    },
+    //
+    onDeviceReady : function () {
+        app.targetEvent = 'touchend';
+        //
+        app.isCordova                                     = (typeof window.cordova !== "undefined");
+        document.getElementById('status').innerHTML       = 'is Cordova';
+        window.screen.orientation.lock('landscape-primary');
+    }
+}
